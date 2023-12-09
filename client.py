988b7114c37cb0b609cf37b2a792207be178c1d5
kaga1minn0nnn4 <toida2003@gmail.com>
2023-12-09 13:30:57 +0900

    refactor

--- client.py
+++ client.py
@@ -47,7 +47,7 @@ def display_result(raw_map, x, y):
     else:
         print("..... YOU LOSE .....")
 
-def player_operate(client):
+def operate(client, mode="player"):
     while True:
         print("Wait other player...")
         response = client.recv(buffer_size).decode()
@@ -69,80 +69,52 @@ def player_operate(client):
         display_map(map_raw, player_x, player_y)
 
         behavior_list = ["w", "a", "d", "s"]
-
         while True:
-            print("Where to move?")
-            dir = input("Select (w, a, s, d) = ")
-            index = None
-            try:
-                index = behavior_list.index(dir)
-            except:
-                print("select w, a, s, d...")
-                continue
+            if mode == "player":
+                print("Where to move?")
+                dir = input("Select (w, a, s, d) = ")
+                index = None
+                try:
+                    index = behavior_list.index(dir)
+                except:
+                    print("select w, a, s, d...")
+                    continue
 
-            if can_behavior[index] == "1":
-                cli_msg = behavior_list[index] + ","
-                break
-            else:
-                print("Select direction is unavailable...")
-
-        if have_item:
-            print("Use item ?")
-            flag = ""
-            while True:
-                flag = input("Select (0, 1) : ")
-                if flag == "0" or flag == "1":
+                if can_behavior[index] == "1":
+                    cli_msg = behavior_list[index] + ","
                     break
                 else:
-                    print("This input is unavailable...")
-            cli_msg += flag + ","
-        else:
-            cli_msg += "0" + ","
-        cli_msg += "0,0"
-
-        client.send(cli_msg.encode())
-
-def npc_operate(client):
-    while True:
-        response = client.recv(buffer_size).decode()
-        res_msgs = response.split(",")
-
-        map_raw = res_msgs[0]
-        can_behavior = res_msgs[1]
-        have_item = int(res_msgs[4])
-        player_x = int(res_msgs[5])
-        player_y = int(res_msgs[6])
-
-        is_end = int(res_msgs[7])
-        if is_end:
-            cli_msg = "0,0,0,1"
-            client.send(cli_msg.encode())
-            display_result(map_raw, player_x, player_y)
-            break
-
-        display_map(map_raw, player_x, player_y)
-
-        behavior_list = ["w", "a", "d", "s"]
-        while True:
-            index = random.randint(0, 3)
-            if can_behavior[index] == "1":
-                cli_msg = behavior_list[index] + ","
-                break
+                    print("Select direction is unavailable...")
+            elif mode == "npc":
+                index = random.randint(0, 3)
+                if can_behavior[index] == "1":
+                    cli_msg = behavior_list[index] + ","
+                    break
 
         if have_item:
-            cli_msg += str(random.randint(0, 1)) + ","
+            if mode == "player":
+                print("Use item ?")
+                flag = ""
+                while True:
+                    flag = input("Select (0, 1) : ")
+                    if flag == "0" or flag == "1":
+                        break
+                    else:
+                        print("This input is unavailable...")
+                cli_msg += flag + ","
+            elif mode == "npc":
+                cli_msg += str(random.randint(0, 1)) + ","
         else:
             cli_msg += "0" + ","
         cli_msg += "0,0"
 
         client.send(cli_msg.encode())
-
 
 
 if __name__ == "__main__":
     args = sys.argv[1:]
     param = {
-        "mode": "client",
+        "mode": "player",
         "server_ip": "127.0.0.1",
         "server_port": 8000
     }
@@ -150,15 +122,15 @@ if __name__ == "__main__":
     for i in range(len(args)):
         param[param_keys[i]] = args[i]
 
+    if not (param["mode"] == "player" or param["mode"] == "npc"):
+        raise
+
     buffer_size = 4096
 
     tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     tcp_client.connect((param["server_ip"], param["server_port"]))
 
-    if param["mode"] == "client":
-        player_operate(tcp_client)
-    else:
-        npc_operate(tcp_client)
+    operate(tcp_client, param["mode"])
 
     tcp_client.close()
